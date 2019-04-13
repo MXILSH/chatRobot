49ec017a640a97cc9a6933468cd293674cc34513
你忙吧t2333 <32233586+MXILSH@users.noreply.github.com>
2019-04-13 23:38:35 +0800

    Add files via upload

--- chat.php
+++ chat.php
@@ -1,6 +1,5 @@
 <?php
 include "ChatTM.php";
-$_BACKGROUND_COLOR = "#2196F3";
 $_TULING_API = "";
 $chat_history = "";
 if(isset($_COOKIE["chathistory"])){
@@ -12,45 +11,45 @@ if(isset($_GET['chat-value'])) {
         //构造实例返回数据
         $chat_obj = new ChatTM($chat_value,$_TULING_API);
         $rt_text = $chat_obj->getReturnText();
-        //判断字数短句？-----------------------------------------------------------
-        //
         //聊天历史
         $chat_history = $chat_history."|".$chat_value."|".$rt_text;
         setcookie("chathistory",$chat_history);
     }
 }
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="mui/css/mui.min.css" rel="stylesheet" type="text/css" />
-    <script src="mui/js/mui.min.js"></script>
-</head>
-<body>
-<div style="width:100%;height:100%;margin: auto">
-    <table width="100%">
-    <?php
-    if(strcmp($chat_history,"") != 0){
-        $temp = explode("|",$chat_history);
-        $i = 1;
-        foreach ($temp as $value){
-            if(strcmp($value,"") == 0){
-                continue;
-            }
-            if($i % 2 != 0){
-                echo "<tr>", "<td align=\"right\"><button class=\"mui-btn\" disabled style=\"background-color:$_BACKGROUND_COLOR;color:white;\">$value</button></td>", "</tr>";
-            }else{
-                echo "<tr>", "<td align=\"left\"><button class=\"mui-btn\" style=\"background-color:$_BACKGROUND_COLOR;color:white;\">$value</button></td>", "</tr>";
-            }
-            $i = $i + 1;
-        }
-    }
-    ?>
-    </table>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
 
-</div>
-</body>
+		<!--标准mui.css-->
+		<link rel="stylesheet" href="mui/css/mui.min.css">
+    </head>
+    <body>
+    <div style="width:100%;height:100%;margin: auto">
+        <table width="100%">
+        <?php
+        if(strcmp($chat_history,"") != 0){
+            $temp = explode("|",$chat_history);
+            $i = 1;
+            foreach ($temp as $value){
+                if(strcmp($value,"") == 0){
+                    continue;
+                }
+                if($i % 2 != 0){
+                    echo "<div align=\"right\"><div class=\"mui-card\" style=\"width:50%\"><div class=\"mui-card-content\"><div class=\"mui-card-content-inner\" style=\"background-color:#2196F3;color:white;\">".$value."</div></div></div></div>";
+                }else{
+                    echo "<div align=\"left\"><div class=\"mui-card\" style=\"width:50%\"><div class=\"mui-card-content\"><div class=\"mui-card-content-inner\" style=\"background-color:#2196F3;color:white;\">".$value."</div></div></div></div>";
+                }
+                $i = $i + 1;
+            }
+        }
+        ?>
+        </table>
+
+    </div>
+    </body>
 </html>
